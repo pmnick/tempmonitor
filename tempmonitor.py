9151b4cb9a0d21efc45e6341ffaa4b720881f8ec
pmnick <nickmorrill3@gmail.com>
2018-11-23 11:08:37 -0700

    Clean up tempmonitor

--- tempmonitor.py
+++ tempmonitor.py
@@ -5,57 +5,50 @@ import spidev
 from time import sleep
 from datetime import datetime, timedelta
 
-print "Start"
 
 spi_0 = spidev.SpiDev()
-#spi_0.bits_per_word = 8
-spi_0.open(0,0) # the second number indicates which SPI pin CE0 or CE1
-#to_send = [0x01,0x02,0x03] #speed Hz, Delay, bits per word
-#spi_0.xfer(to_send)
-print "here"
+spi_0.open(0, 0) # the second number indicates which SPI pin CE0 or CE1
 
-def readadc_0(chan):
+def read_adc(chan):
     if chan > 7 or chan < 0:
         return -1
 
-    #r_0 = spi_0.xfer([1,8 + adcnum_0 << 4, 0]) # start bit, Single/Differntial mode, don't care bit OR
-    #r_0 = spi_0.xfer([1,128 + (adcnum_0 << 3),0])
-    #r_0 = spi_0.xfer([0x84,0x00])#132 and 196
-    # r_0 = spi_0.xfer([0b10000100,0]) # binary literal '0b' + 7 bit input data-word + 0 (not sure why the 0),
-    r_0 = spi_0.xfer([132 + 64 * (chan % 2) + 16 * chan // 2, 0]) # come up with a cleaner bitwise equivalent
-    # print r_0
+    # see table on pg. 9 https://www.analog.com/media/en/technical-documentation/data-sheets/LTC1863L-LTC1867L.pdf
+    # for explanation of channel to bit equivalent
+    r_0 = spi_0.xfer([132 + 64 * (chan % 2) + 16 * chan // 2, 0]) # TODO come up with a cleaner bitwise equivalent?
     adcout_0 = ((r_0[0] & 255) << 8) + r_0[1]
-    #adcout_0 = (r_0[0] << 8) + r_0[1]
-    #print r_0
     return adcout_0
 
+# convert voltage to degrees C (this is based on a quick and dirty two point measurement using cold and hot water)
+# temp sensor is NPN transistor, measuring voltage drop between emitter and drain as described here:
+# https://www.sensortips.com/featured/get-temperature-sensor-transistor/
+# current source is actually a voltage divider from 12v to about 1.15 v, actual conversion is probably nonlinear
+# because this is a very non-ideal current source. TODO: find calibration curve using 4+ points
+def to_C(voltage):
+    return -470 * voltage + 313
 
-def to_C(reading):
-    return -470 * reading + 313
-
-buffer = []
+buf = []
 bsize = 10
 sample_period = timedelta(seconds=10)
+read_period = 0.2/10 * sample_period.total_seconds() # sample is average of 10 readings during last 20% of sample period
 
 try:
-    reading = to_C(round(1.0*readadc_0(0)/65535*3.3, 6))
-    print ('current temp: {}'.format(reading))
+    reading = to_C(round(1.0*read_adc(0)/65535*3.3, 6))
+    print('current temp: {}'.format(reading))
     last_entry = datetime.now()
 
     while True:
-        reading = to_C(round(1.0*readadc_0(0)/65535*3.3, 6))
-        buffer = buffer[-(bsize-1):] + [reading]
+        reading = to_C(round(1.0*read_adc(0)/65535*3.3, 6))
+        buf = buf[-(bsize-1):] + [reading]
 
         now = datetime.now()
         if now - last_entry > sample_period:
             with open('temp_log_2.txt', 'a') as f:
-                f.write('{}, {}\n'.format(now.isoformat(), sum(buffer)/len(buffer)))
+                f.write('{}, {}\n'.format(now.isoformat(), sum(buf)/len(buf)))
             last_entry = now
-	sleep(.5)
-	
-except KeyboardInterrupt:
-    print "interupted"
+    sleep(read_period)
 
-print "here"
+except KeyboardInterrupt:
+    pass
 
 spi_0.close()
